fe98098848057f0462ae4280afb8d5d04811365d
Khyzar Kapoor <khyzar@protonmail.com>
2019-04-18 17:26:13 +0500

    Staging

--- config/database.js
+++ config/database.js
@@ -1,6 +1,5 @@
 module.exports = {
     // database: 'mongodb://localhost:27017/konstantvariables',
-    // database:'mongodb+srv://khyzar:khyzar@konstantvariables-fkxcs.mongodb.net/test?retryWrites=false',
     database:'mongodb://konstantvariable:konstant1@ds143666.mlab.com:43666/konstantvariables',
     secret: 'KonstantVariables@Assignment_'
 }
